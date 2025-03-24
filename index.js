a0ecafac8d3fd3e94259484a1ee64595e7a0199e
Ama Nkansah <amankansahstate@gmail.com>
2025-03-24 22:17:11 +0000

    JS issue fixed on the nesting of hr element

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
-  $("img.image-class").on("mousemove touchmove", function () {
-    $(this).nextAll("hr").first().removeClass("hidden").addClass("flex");
+  $("img.image-class").on("mouseover touchmove", function () {
+    console.log("Event triggered"); //debugging statement
+
+    const hrElement = $(this).siblings("div").find("hr");
+
+    hrElement.removeClass("hidden").addClass("flex");
   });
 });
 
