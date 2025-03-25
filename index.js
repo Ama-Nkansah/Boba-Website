4aa884a94e91a0996ffc50b0254523a4b4897235
Ama Nkansah <amankansahstate@gmail.com>
2025-03-25 00:52:36 +0000

    zoomin animation done now text-pop

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
   $("img.image-class").on("mouseover touchmove", function () {
     console.log("Event triggered"); //debugging statement
-
+    
     const hrElement = $(this).siblings("div").find("hr");
+    const image_animation = $(this);
+
+   //reset all elements
+   $("hr").addClass("hidden").removeClass("flex"); 
+   $("img.image-class").removeClass("animate-zoom");  
+
+    //triggers event for the specific img
     hrElement.removeClass("hidden").addClass("flex");
+    image_animation.addClass("animate-zoom");
+ 
+    //what to do after animation
+    setTimeout(()=>{
+      image_animation.removeClass("animate-zoom");
+    },900); //time must match css time
+
   });
 });
-
-
-
